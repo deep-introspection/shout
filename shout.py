139ccbbf686d1b581ee307019c85da8d282dc081
deep-introspection <deep@introspection.eu>
2016-03-16 13:27:00 +0100

    Initial commit for the shout module

---
+++ shout.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# ==============================================================================
+# title           : shout.py
+# description     : This will give a function to shout messages.
+# author          : Guillaume Dumas
+# date            : 2016
+# version         : 0.1a
+# usage           : in python, use "import shout"
+# notes           : Work in progress ...
+# python_version  : 2.7, 3.5
+# ==============================================================================
+
+
+def shout(msg="Hello world!"):
+    print(msg.upper())
